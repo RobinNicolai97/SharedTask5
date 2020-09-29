8a922ecff019e8acab382b8ddc9c38159c74258b
RobinNicolai97 <48721583+RobinNicolai97@users.noreply.github.com>
2020-09-29 14:28:19 +0200

    A start on the program

---
+++ baseline.py
@@ -0,0 +1,21 @@
+import csv
+
+with open("tsd_trial.csv",  encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0          
+    bad_words_file = open("badwords.txt", encoding="utf8")
+    bad_words_lib = [line.strip() for line in bad_words_file.readlines()] 
+    for row in csv_reader:
+            print("in the sentence: {0}, spans: {1} are toxic \n".format(row[1], row[0])) 
+            matches = [ word for word in row[1].split() if word in bad_words_lib] 
+            print('Found the bad words:', matches, '\n')
+            print('Which are spans:')
+            for match in matches:
+                position = row[1].find(match)
+                span = []
+                for i in range(len(match)):
+                    span.append(position + i )
+                print(span, '\n')
+                
+            
+            
