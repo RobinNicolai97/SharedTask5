7f7b0a4489c88895b7be8b857e32cd7487814157
RobinNicolai97 <48721583+RobinNicolai97@users.noreply.github.com>
2020-11-17 15:07:04 +0100

    added an option to also add full toxic sents

--- baseline.py
+++ baseline.py
@@ -10,10 +10,10 @@ import re
 #   Please double check if in these cases, the counting of the offsets of the characters is correctly implemented 
 #   Double checked - Works ( SEE TEXTFILE WITH PROOF)
 #
-
+answer = input('Do you want to use full toxic messages (empty brackets)? y/n: ')
 total_scores = [0,0,0] #correct finds, missed finds, incorrect finds
 row = 0
-with open("tsd_train.csv",  encoding="utf8") as csv_file:
+with open("tsd_trial.csv",  encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',') # read csv-file
     line_count = 0          
     bad_words_file = open("badwords.txt", encoding="utf8") #loading a library of bad words to identify toxicity
@@ -33,10 +33,11 @@ with open("tsd_train.csv",  encoding="utf8") as csv_file:
             right_answers = row[0].strip('][').split(', ') 
             if len(right_answers) == 1: #all characters of messages belong to the toxic span if list is empty
                 right_answers = []
-                #i = 0
-                #for char in row[1]:
-                  #  right_answers.append(i)
-                   # i +=1  
+                if answer.lower() == 'y':
+                    i = 0
+                    for char in row[1]:
+                        right_answers.append(i)
+                        i +=1  
             right_answers = [int(ans) for ans in right_answers] #make sure all answers are integers
             #checking for true positives, false positives and false negatives. 
             correct = 0
