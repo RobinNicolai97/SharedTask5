e868770ea3062733a232411054de4f8e1adf9a05
RobinNicolai97 <48721583+RobinNicolai97@users.noreply.github.com>
2020-12-03 13:26:21 +0100

    changes, urls work now

--- BIO.py
+++ BIO.py
@@ -8,13 +8,54 @@ with open("tsd_train.csv",  encoding="utf8") as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',') # read csv-file 
 	for row in csv_reader:
 		if row[0].strip() == '[]':
-			count = 0
-			for word in word_tokenize(row[1]):
-				if count == 0:
-					f.write(word + '\t' + 'B-toxic\n')
+			i = 0
+			tag = ''
+			prev_tag = ''
+			string = ''
+			for char in row[1]:
+				if char == ' ':
+					if tag == 'B-toxic' and ( prev_tag == 'B-toxic' or prev_tag == 'I-toxic' or prev_tag == 'newword'):
+						tag = 'I-toxic'
+					if tag != 'newword':
+						f.write('\t'+tag+'\n')	
+					string = ''
+					prev_tag = tag
+					tag = 'newword'
+				elif char == '\n':
+					if tag == 'B-toxic' and ( prev_tag == 'B-toxic' or prev_tag == 'I-toxic' or prev_tag == 'newword'):
+						tag = 'I-toxic'
+					if tag != 'newword':
+						f.write('\t'+tag+'\n')	
+					prev_tag = tag
+					tag = 'newword'
+					string = ''
+					
 				else:
-					f.write(word + '\t' + 'I-toxic\n')
-				count += 1
+					if not char.isalpha():
+						if 'https' in string[0:8]:
+							tag = 'B-toxic'
+							f.write(char)
+						else:
+							if tag == 'B-toxic' and ( prev_tag == 'B-toxic' or prev_tag == 'I-toxic' or prev_tag == 'newword'):
+								tag = 'I-toxic'
+							if tag != 'newword':
+								f.write('\t'+tag+'\n')
+							string = ''
+							prev_tag = tag
+							tag = 'B-toxic'
+							f.write(char)								
+					else:	
+						tag = 'B-toxic'
+						f.write(char)
+				string += char
+			if tag == 'B-toxic' and ( prev_tag == 'B-toxic' or prev_tag == 'I-toxic' or prev_tag == 'newword'):
+				tag = 'I-toxic'
+			f.write('\t'+tag+'\n')	
+			prev_tag = ''
+			tag = '' 
+			string = ''
+			f.write('\n')
+			
 			f.write('\n') 
 		elif row[0].strip() == 'spans':
 				pass 
